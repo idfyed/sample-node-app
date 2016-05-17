3d82e05b2bf277c4592c55c7b6131a9a3237caed
Jonas Andreasson <jonas@diglias.com>
2016-05-17 15:58:03 +0200

    Created a grunt task 'dist' that builds a zip file that can be distributed to customers.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@
  * 
  */
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     grunt.initConfig({
 
@@ -13,12 +13,9 @@ module.exports = function(grunt) {
 
         jshint: {
             files: ['Gruntfile.js',
-                'app.js',
-                'bin/**/*.js',
-                'diglias/**/*.js',
-                'lab/**/*.js',
-                'routes/**/*.js',
-                'www/**/*.js'
+                'src/**/*.js',
+                'eapi-client/src/**/*.js',
+                'eapi-client/test/**/*.js',
             ],
             options: {
                 node: true,
@@ -44,14 +41,61 @@ module.exports = function(grunt) {
                     spawn: false
                 }
             }
-        }
+        },
+
+
+        copy: {
+            zip: {
+                files: [
+                    { expand: true, src: ['eapi-client/*.tgz'], dest: 'diglias-node-sample-app/' },
+                    { expand: true, src: ['src/**/*'], dest: 'diglias-node-sample-app/' },
+                    {
+                        expand: true,
+                        src: [
+                            '.dockerignore',
+                            'docker-compose.yml',
+                            'Dockerfile',
+                            'package.json',
+                            'README.md'
+                        ],
+                        dest: 'diglias-node-sample-app/'
+                    },
+
+                ],
+            },
+        },
+
+        compress: {
+            zip: {
+                options: {
+                    archive: 'diglias-node-sample-app.zip'
+                },
+                files: [
+                    { src: ['diglias-node-sample-app/**'], dest: '/' },
+                ]
+            }
+        },
+
+        clean: {
+            zip: ['diglias-node-sample-app'],
+            all: [
+                'node_modules',
+                'eapi-client/node_modules',
+                'diglias-node-sample-app.zip'
+                ],
+        },
+
 
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.registerTask('lint', ['Run JSHint on all source and test files'], 'jshint');
 
     grunt.registerTask('default', ['lint']);
+    grunt.registerTask('dist', ['copy:zip', 'compress:zip', 'clean:zip']);
 };
