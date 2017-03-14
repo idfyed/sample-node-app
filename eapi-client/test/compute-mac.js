9cda68078cee45a70ca0d67a5bfa776f0c2772ac
Jonas Andreasson <jonas@diglias.com>
2017-03-14 18:29:42 +0100

    Test for different macs for different alphabetical order of parameter values

--- eapi-client/test/compute-mac.js
+++ eapi-client/test/compute-mac.js
@@ -12,6 +12,45 @@ var computeMac = require('../lib/compute-mac');
 var testData = require('./test-data');
 
 describe('#compute-mac', function () {
+  it('should compute different MACS for different alphabetical orders', function () {
+    var mac1 = computeMac(
+      /* eslint-disable camelcase */
+      {auth_parameter: 'a,b,c'},
+      /* eslint-enable camelcase */
+      testData.key
+    );
+
+    var mac2 = computeMac(
+      /* eslint-disable camelcase */
+      {auth_parameter: 'c,b,a'},
+      /* eslint-enable camelcase */
+      testData.key
+    );
+
+    assert.notEqual(mac1, mac2);
+  });
+  it('should only include auth_ parameters in the mac', function () {
+    var mac1 = computeMac(
+      {
+        /* eslint-disable camelcase */
+        auth_parameter: 'value',
+        other_parameter: 'value1'
+        /* eslint-enable camelcase */
+      },
+      testData.key);
+
+    var mac2 = computeMac(
+      {
+        /* eslint-disable camelcase */
+        auth_parameter: 'value',
+        other_parameter: 'different'
+        /* eslint-enable camelcase */
+      },
+      testData.key);
+
+    assert.equal(mac1, mac2);
+  });
+
   it('should return the expected response MAC', function () {
     assert.equal(testData.responseMAC, computeMac(testData.responseData, testData.key));
   });
