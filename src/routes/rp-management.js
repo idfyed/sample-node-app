c91ace0e19f2d30316850dc04ca3aed9a9f015d9
Jonas Andreasson <jonas@diglias.com>
2017-03-22 13:08:30 +0100

    RP Management call takes endpoint config from config file

--- src/routes/rp-management.js
+++ src/routes/rp-management.js
@@ -14,62 +14,77 @@ var request = require('request');
 var c = require('./common');
 
 /**
+ * Base URLs for api endpoints
+ */
+var envEndpoints = {
+    prod: 'https://api.diglias.com/rp-mgmt/attribute/v1.0/',
+    test: 'https://test.diglias.com/rp-mgmt/attribute/v1.0/'
+};
+
+/**
  * Call the Diglias service trough a backend channel and add a attribute
  * to the Diglias users profile.
  */
 
 router.post('/', function (req, res, next) {
 
-  // Load relying party cofiguration from file
-  var conf = c.loadDigliasConf().rpManagement;
+    // Load relying party cofiguration from file
+    var conf = c.loadDigliasConf();
 
-  // Create a request body according to API specification
-  var requestBody = {
-    action: 'ADD',
-    attributes: [
-      {
-        name: 'playground_string',
-        value: req.body.value
-      }
-    ]
-  };
+    // Create a request body according to API specification
+    var requestBody = {
+        action: 'ADD',
+        attributes: [
+            {
+                name: 'playground_string',
+                value: req.body.value
+            }
+        ]
+    };
 
-  var options = {
-    auth: {                     // Use basic authentication to access
-      user: conf.user,          // the RP Management API
-      pass: conf.secret,
-      sendImmediately: true
-    },
-    headers: {                  // Body to be sent as JSON
-      ContentType: 'application/json'
-    },
-    json: true,
-    body: requestBody
-  };
+    var options = {
+        auth: {                     // Use basic authentication to access
+            user: conf.rpManagement.user,          // the RP Management API
+            pass: conf.rpManagement.secret,
+            sendImmediately: true
+        },
+        headers: {                  // Body to be sent as JSON
+            ContentType: 'application/json'
+        },
+        json: true,
+        body: requestBody
+    };
 
-  // The base of the API endpoint
-  var endpoint = 'https://test.diglias.com/rp-mgmt/attribute/v1.0/';
+    //
+    // Validate that the configuration conatins a valid endpoint.
 
-  // add on the company name
-  endpoint = endpoint.concat(conf.companyname);
-
-  // and the user userid
-  endpoint = endpoint.concat('/').concat(req.body.userid);
-
-  request.post(endpoint, options, function (err, result) {
-      if (err) {
-        throw (err);
-      }
-
-      // The expected returned status code is 204 - without a body,
-      // anything else is a error.
-      if (result.statusCode != 204) {
-        res.render('rp-management-error', result);
-      } else {
-        res.render('rp-management-success', { value: req.body.value });
-      }
+    if (typeof envEndpoints[conf.endPoint] === 'undefined') {
+        throw new Error('Invalid endpoint value: ' + endpoint);
     }
-  );
+
+    // The base of the API endpoint
+    var endpoint = envEndpoints[conf.endPoint];
+
+    // add on the company name
+    endpoint = endpoint.concat(conf.rpManagement.companyname);
+
+    // and the user userid
+    endpoint = endpoint.concat('/').concat(req.body.userid);
+
+    request.post(endpoint, options, function (err, result) {
+            if (err) {
+                throw (err);
+            }
+
+            // The expected returned status code is 204 - without a body,
+            // anything else is a error.
+            if (result.statusCode != 204) {
+                res.render('rp-management-error', result);
+            } else {
+                res.render('rp-management-success', {value: req.body.value});
+            }
+        }
+    );
 });
 
 module.exports = router;
