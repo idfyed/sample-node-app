e73af97ea16b438605bc0ae77f197e72231ba495
jakeIDFyed <jake@idfyed.com>
2019-04-23 14:32:54 +0800

    Remove declaration of gulp-nsp from eapi-client/gulpfile.js

--- eapi-client/gulpfile.js
+++ eapi-client/gulpfile.js
@@ -6,7 +6,6 @@ var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 
 gulp.task('static', function () {
